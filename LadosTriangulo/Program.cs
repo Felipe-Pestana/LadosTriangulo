d141d285fe2240ca58127e68b439d932c45bffc6
felipe.pestana <felipe.pestana@sitic.com.br>
2025-10-08 10:56:44 -0300

    Add project files.

---
+++ LadosTriangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿float lado1, lado2, lado3;
+Console.WriteLine("Digite o valor do lado 1:");
+lado1 = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Digite o valor do lado 2:");
+lado2 = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Digite o valor do lado 3:");
+lado3 = float.Parse(Console.ReadLine()!);
+
+if ((lado1 < (lado2 + lado3)) && (lado2 < (lado1 + lado3)) && (lado3 < (lado1 + lado2)))
+{
+    if ((lado1 == lado2) && (lado2 == lado3))
+    {
+        Console.WriteLine("Triangulo Equilatero!");
+    }
+    else if ((lado1 == lado2) || (lado2 == lado3) || (lado1 == lado3))
+    {
+        Console.WriteLine("Triangulo Isosceles");
+    }
+    else
+    {
+        Console.WriteLine("Triangulo Escaleno");
+    }
+}
+else
+{
+    Console.WriteLine("Os valores informados não formam um triângulo.");
+    return;
+}

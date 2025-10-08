e666bc406717c0ecf66a620bedc04883fe8446be
felipe.pestana <felipe.pestana@sitic.com.br>
2025-10-08 15:52:41 -0300

    Correções dos exercícios

---
+++ ParImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿int numero, pares = 0, impares = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite um numero inteiro: ");
+    numero = int.Parse(Console.ReadLine()!);
+    if (numero % 2 == 0){
+        Console.WriteLine("O numero é par!");
+        pares++;
+    }
+    else {
+        Console.WriteLine("O numero é impar!");
+        impares++;
+    }
+
+}
+    Console.WriteLine("Quantidade de numeros pares: " + pares);
+    Console.WriteLine($"Quantidade de numeros impares: {impares}");
---
+++ Sorteio/Program.cs
@@ -0,0 +1,18 @@
+﻿int palpite, sorteado;
+
+
+Random aleatorio = new Random();
+sorteado = aleatorio.Next(1, 51);
+
+do
+{
+    Console.WriteLine("Informe o numero do palpite: ");
+    palpite = int.Parse(Console.ReadLine()!);
+
+    if(palpite < sorteado)
+        Console.WriteLine("Errou! O palpite é menor que o numero sorteado!");
+    else if(palpite > sorteado)
+        Console.WriteLine("Errou! O palpite é maior que o numero sorteado!");
+
+} while (palpite != sorteado);
+Console.WriteLine("Parabéns, você acertou o número sorteado: " + sorteado);
